7ba433e331ed33b5542e961782e9a9bd3421d21f
linyoulai <145471659+linyoulai@users.noreply.github.com>
2023-11-29 19:22:50 +0800

    added

--- dance.cpp
+++ dance.cpp
@@ -1,46 +1,4 @@
-#include <stdio.h>
-#include <Windows.h>
-#include <iostream>
-using namespace std;
-//播放声音的头文件
-#include <mmsyscom.h>
-#pragma comment(lib, "winmm.lib")
-#include <opencv2/opencv.hpp>
-using namespace cv; //声明opencv的命名空间
-int main(void) {
-	//1、打开视频文件
-	//下载一个mp4视频，与源文件同目录；
-	//将mp4转为mp3，与源文件同目录，因为视频转为字符就没有声音了；
-	VideoCapture video;
-	bool ret = video.open("楼梯科目三.mp4");//ret = return
-	if (ret == false) {
-		printf("视频文件打开失败！┭┮﹏┭┮\n");
-	}
-	else {
-		printf("视频文件打开成功！(*^▽^*)\n");
-	}
-	//2、读取视频的核心信息（有多少帧，视频的宽度，高度，播放的速度）
-	video.get(CAP_PROP_FRAME_COUNT);
-	//帧数
-	int frame_count = video.get(CAP_PROP_FRAME_COUNT);
-	int fps = video.get(CAP_PROP_FPS);
-	//宽高
-	int cols = video.get(CAP_PROP_FRAME_WIDTH);
-	int rows = video.get(CAP_PROP_FRAME_HEIGHT);
-	//定义抽样的尺度
-	int w_size = 9;
-	int h_size = 20;
-	//准备 每10行，每5列抽取一个点，转换成一个字符
-	//原理：很白 -> 空格
-	char char_imgs[] = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@@@@@@@@@@@@@@@";//0 - 255,分成69级
-	int len = sizeof(char_imgs) / sizeof(char_imgs[0]);
-	//每个像素点的灰度值 除以20 作为字符的序号
 
-	//控制台的窗口
-	int width = cols / w_size;
-	int height = rows / h_size;
-	// mode con cols = 100 lines = 80
-	char cmd[128];
 	sprintf_s(cmd, sizeof(cmd), "mode con cols = %d lines = %d", width, height);
 	system(cmd);
 
