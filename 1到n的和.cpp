37249ddd3e7b80807e08d9d119c1279551fb00c9
linyoulai <145471659+linyoulai@users.noreply.github.com>
2023-11-29 19:08:14 +0800

    Add files via upload

---
+++ 1到n的和.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int fun(int n) {
+//	if (n == 1)
+//	{
+//		return 1;
+//	}
+//	return n + fun(n - 1);
+//}
+//int main() {
+//	int n = 0;
+//	cin >> n;//n>0
+//
+//	cout << fun(n);
+//	return 0;
+//}
